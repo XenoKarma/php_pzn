c2e8d825b388acd9979a9099b9675a3e2adafa4f
XenoKarma <andikapurnama1803@gmail.com>
2025-06-11 22:51:27 +0700

    five-commit

--- belajar-php-dasar/Constant.php
+++ belajar-php-dasar/Constant.php
@@ -7,3 +7,4 @@ echo "Author: " . AUTHOR . "\n";
 echo "App Version: " . APP_VERSION . "\n";
 // APP_VERSION = 200; // ini akan error karena constant tidak bisa diubah
 // define("APP_VERSION", 200); // ini juga akan error karena constant tidak bisa didefinisikan ulang
+// define 
